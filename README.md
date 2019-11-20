be72a6b36686942432f60f5157f3f4c3f92d4f54
Dao Minh Thuc <minhthucdao1@gmail.com>
2019-11-20 11:40:31 +0700

    Add initial code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clickhouse-inserter-from-kafka
+
+Consume JSON data from a topic and insert it into a clickhouse table, supporting nested data structure
+
---
+++ clickhouse_inserter_from_kafka.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from kafka import KafkaConsumer
+import click
+from requests import Request, Session
+
+@click.command()
+@click.argument('kafka_broker')
+@click.argument('topic')
+@click.argument('group')
+@click.argument('clickhouse_server')
+@click.argument('table')
+
+def consume_and_insert(kafka_broker, topic, group, clickhouse_server, table):
+    url = clickhouse_server + "/?query=INSERT%20INTO%20" + table + "%20FORMAT%20JSONEachRow" 
+    data = b''
+    s = Session()
+    consumer = KafkaConsumer(topic, bootstrap_servers = kafka_broker, enable_auto_commit = False, group_id = group)
+    for message in consumer:
+        data += message.value
+        if len(data) > 1000000:
+            resp = s.post(url, data=data)
+            if resp.status_code == 200:
+                consumer.commit()
+            else:
+                print("fail to insert into clickhouse, quit") 
+                return
+    return
+
+
+if __name__ == "__main__":
+    consume_and_insert()
+
